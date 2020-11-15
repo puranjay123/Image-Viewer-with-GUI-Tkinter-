71f5862716b078a97e457d854a11079ba8842f4d
puranjay123 <55429956+puranjay123@users.noreply.github.com>
2020-11-16 04:22:53 +0530

    Add files via upload

--- image_Viewer_Project.py
+++ image_Viewer_Project.py
@@ -12,6 +12,7 @@ my_img5 =ImageTk.PhotoImage(Image.open(r"C:\Users\Asus\Desktop\Tkinter YT\moon5.
 
 image_list = [my_img1,my_img2,my_img3,my_img4,my_img5]
 #image_list[]
+status =Label(root,text="Imageof"+str(len(image_list)), bd=0.8,relief=SUNKEN,anchor=E)
 
 my_label =Label(image=my_img1)
 my_label.grid(row=0,column=0,columnspan=3)
@@ -32,7 +33,10 @@ def forward(image_number) :
     button_forward.grid(row=1,column=2)
      
     my_label.grid(row=0,column=0,columnspan=3)
+    status =Label(root,text="Image"+ str(image_number)+"of "+str(len(image_list)), bd=0.8,relief=SUNKEN,anchor=E)
+    status.grid(row=2,column=0,columnspan=3,sticky=W+E)
     
+
     
 def back(image_number) :
     global my_label
@@ -42,13 +46,16 @@ def back(image_number) :
     my_label = Label(image = image_list[image_number-1])
     button_forward =Button(root,text='<<',command=lambda : forward(image_number+1))
     button_back =Button(root,text='>>',command=lambda : back(image_number-1))
-    
+    if image_number == 1 :
+        button_back=Button(root,text='<<',state=DISABLED)
     button_back.grid(row=1,column=0)
     button_forward.grid(row=1,column=2)
      
     my_label.grid(row=0,column=0,columnspan=3)
-    if image_number == 1 :
-        button_back=Button(root,text='<<',state=DISABLED)
+    
+    status =Label(root,text="Image"+ str(image_number)+"of "+str(len(image_list)), bd=0.8,relief=SUNKEN,anchor=E)
+    status.grid(row=2,column=0,columnspan=3,sticky=W+E)
+        
     
 button_back =Button(root,text='<<',command=back,state=DISABLED)
 button_quit=Button(root,text="Exit program",command=root.quit)
@@ -57,10 +64,10 @@ button_forward =Button(root,text='>>',command=lambda :forward(2))
 
 button_back.grid(row=1,column=0)
 button_quit.grid(row=1,column=1)
-button_forward.grid(row=1,column=2)
+button_forward.grid(row=1,column=2,pady=10)
 
 
-
+status.grid(row=2,column=0,columnspan=3,sticky=W+E)
 
 
 root.mainloop()
